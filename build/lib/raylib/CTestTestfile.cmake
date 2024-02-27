1834e05a31693d9cdd2911b4e2dd424c09a95995
jusoue <67708393+jusoue@users.noreply.github.com>
2024-02-26 21:22:09 -0500

    Delete build directory

--- build/lib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/mastermind/lib/raylib
-# Build directory: C:/dev/mastermind/build/lib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
--- build/lib/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/mastermind/lib/raylib/src
-# Build directory: C:/dev/mastermind/build/lib/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
